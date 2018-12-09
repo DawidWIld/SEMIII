26fa96ceddf1c95ae31002c0d9cd19cb72e47a76
Timu5 <timusziomek@gmail.com>
2018-12-09 17:51:23 +0100

    Fix inverse fft

--- MMI/fft.py
+++ MMI/fft.py
@@ -1,5 +1,4 @@
 import cmath
-from numpy.fft import fft, ifft
 
 a = [2,1,3,1,3,0,0,0,0,0,0,0,0,0,0,0]
 b = [1,1,1,8,0,0,0,0,0,0,0,0,0,0,0,0]
@@ -17,33 +16,27 @@ def slowmul(a, b):
             c[x+y] += a[x] * b[y]
     return c
 
-def mfft(x):
-    N = len(x)
-    if N <= 1:
+def fft(x):
+    n = len(x)
+    if n <= 1:
         return x
-    even = mfft(x[0::2])
-    odd = mfft(x[1::2])
-    T = [cmath.exp(-2j*cmath.pi*k/N)*odd[k] for k in range(N//2)]
-    return [even[k] + T[k] for k in range(N//2)] + [even[k] - T[k] for k in range(N//2)]
+    even = fft(x[0::2])
+    odd = fft(x[1::2])
+    T = [cmath.exp(-2j*cmath.pi*k/n)*odd[k] for k in range(n//2)]
+    return [even[k] + T[k] for k in range(n//2)] + [even[k] - T[k] for k in range(n//2)]
 
-def minvfft(x):
-    return [(1/len(x) * i).conjugate() for i in mfft(x)]
+def ifft(x):
+    return [(1/len(x) * i).conjugate() for i in fft([i.conjugate() for i in x])]
 
 def fftmul(a, b):
     fa = fft(a)
     fb = fft(b)
     fc = [fa[i] * fb[i] for i in range(len(a))]
-    return [c.real for c in minvfft(fc)]
+    tmp = [c.real for c in ifft(fc)]
+    return tmp
 
 print(slowmul(a,b))
 print(fftmul(a,b))
 
 print(calc(slowmul(a,b), 1j + 3))
 print(calc(fftmul(a,b), 1j + 3))
-'''
-print(minvfft(mfft(a)))
-print()
-print(ifft(a))
-print()
-print(minvfft(a))
-'''
